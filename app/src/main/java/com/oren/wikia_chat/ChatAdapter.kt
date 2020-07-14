a69164d22e15833ef6696010956d0ab5fca84740
turicfr <turicfr@gmail.com>
2020-07-15 01:27:58 +0300

    Improve participants feature

--- app/src/main/java/com/oren/wikia_chat/ChatAdapter.kt
+++ app/src/main/java/com/oren/wikia_chat/ChatAdapter.kt
@@ -1,19 +1,14 @@
 package com.oren.wikia_chat
 
 import android.content.Context
-import android.graphics.*
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-
 import com.squareup.picasso.Picasso
-import com.squareup.picasso.Transformation
-
 import kotlin.math.abs
-import kotlin.math.min
 
 class ChatAdapter(private val mContext: Context, private val mChatItems: List<ChatItem>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -42,7 +37,10 @@ class ChatAdapter(private val mContext: Context, private val mChatItems: List<Ch
                 viewHolder as MessageViewHolder
                 viewHolder.username = message.username
                 viewHolder.message = message.messages.joinToString("\n")
-                Picasso.get().load(message.avatarSrc).transform(CircleTransform()).into(viewHolder.avatar)
+                Picasso.get()
+                    .load(message.avatarSrc)
+                    .transform(CircleTransform())
+                    .into(viewHolder.avatar)
             }
             ChatItem.TYPE_LOG -> {
                 message as ChatItem.Log
@@ -95,38 +93,5 @@ class ChatAdapter(private val mContext: Context, private val mChatItems: List<Ch
             set(value) {
                 mMessageView.text = value
             }
-    }
-
-    class CircleTransform : Transformation {
-        override fun transform(source: Bitmap): Bitmap {
-            val size = min(source.width, source.height)
-
-            val x = (source.width - size) / 2
-            val y = (source.height - size) / 2
-
-            val squaredBitmap = Bitmap.createBitmap(source, x, y, size, size)
-            if (squaredBitmap != source) {
-                source.recycle()
-            }
-
-            val bitmap = Bitmap.createBitmap(size, size, source.config)
-
-            val canvas = Canvas(bitmap)
-            val paint = Paint().apply {
-                shader = BitmapShader(
-                    squaredBitmap,
-                    Shader.TileMode.CLAMP, Shader.TileMode.CLAMP
-                )
-                isAntiAlias = true
-            }
-
-            val r = size / 2f
-            canvas.drawCircle(r, r, r, paint)
-
-            squaredBitmap.recycle()
-            return bitmap
-        }
-
-        override fun key() = "circle"
     }
 }
--- app/src/main/java/com/oren/wikia_chat/UsersAdapter.kt
+++ app/src/main/java/com/oren/wikia_chat/UsersAdapter.kt
@@ -3,8 +3,10 @@ package com.oren.wikia_chat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
 class UsersAdapter(private val mUsers: List<User>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -17,13 +19,19 @@ class UsersAdapter(private val mUsers: List<User>) :
 
     override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
         val user = mUsers[position]
-        (viewHolder as UserViewHolder).username = user.name
+        viewHolder as UserViewHolder
+        viewHolder.username = user.name
+        Picasso.get()
+            .load(user.avatarUri)
+            .transform(CircleTransform())
+            .into(viewHolder.avatar)
     }
 
     override fun getItemCount() = mUsers.size
 
     class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val mUsernameView = itemView.findViewById<TextView>(R.id.username)
+        val avatar = itemView.findViewById<ImageView>(R.id.avatar)
 
         var username: String
             get() = mUsernameView.text.toString()
