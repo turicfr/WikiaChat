ec5148889f4f33055f02a7c9c8fbf205221d4040
turicfr <turicfr@gmail.com>
2020-07-29 15:41:56 +0300

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,10 +38,10 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(kotlin("stdlib-jdk7", KotlinCompilerVersion.VERSION))
 
-    implementation("androidx.core:core-ktx:1.3.0")
+    implementation("androidx.core:core-ktx:1.3.1")
     implementation("androidx.appcompat:appcompat:1.1.0")
     implementation("androidx.recyclerview:recyclerview:1.1.0")
-    implementation("androidx.fragment:fragment-ktx:1.3.0-alpha06")
+    implementation("androidx.fragment:fragment-ktx:1.3.0-alpha07")
     implementation("androidx.preference:preference:1.1.1")
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.1.0")
     implementation("com.google.android.material:material:1.1.0")
