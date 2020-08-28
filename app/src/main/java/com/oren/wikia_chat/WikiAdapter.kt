f6d46b0a60c495bcbae4d619faf1a89d5588e59b
turicfr <turicfr@gmail.com>
2020-08-28 20:05:42 +0300

    Rename chat_item to wiki_item

--- app/src/main/java/com/oren/wikia_chat/WikiAdapter.kt
+++ app/src/main/java/com/oren/wikia_chat/WikiAdapter.kt
@@ -27,7 +27,7 @@ class WikiAdapter(private var mWikis: MutableList<Wiki>) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
         LayoutInflater
             .from(parent.context)
-            .inflate(R.layout.chat_item, parent, false)
+            .inflate(R.layout.wiki_item, parent, false)
     )
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
